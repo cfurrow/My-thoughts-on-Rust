e042ab3bd67cd5eba76772f0b8bfd9f167aed7f7
Carl Furrow <me@carlfurrow.com>
2012-08-02 10:27:58 -0400

    Update code to reflect Rust 0.3.1 changes

--- src/fib.rs
+++ src/fib.rs
@@ -2,25 +2,23 @@ import io::println;
 
 // Some examples show that you can use "args: ~[~str]"
 //   but I got nothing but errors with that syntax.
-fn main(args:[str]){
+fn main(args:~[~str]){
   // Having to define vars as muteable will get old quick.
-  let mut i: int = 0;
-  let mut max: int = 10;
-  let mut sum: int = 0;
-  let mut last: int = 0;
-  let mut curr: int = 1;
+  let mut i    = 0;
+  let mut max  = 10;
+  let mut sum  = 0;
+  let mut last = 0;
+  let mut curr = 1;
   // Cannot check against just "1" because type of .len() is unsigned
   if( args.len() > 1u){
-    // Well this is weird.
-    //  int::from_str returns core::option::option<int>
-    //  to get the value, have to use options::get() 
-    max = option::get(int::from_str(args[1]));
+
+    max = int::from_str(args[1]).get();
   }
 
   while(i < max){
    // println cannot directly print an int, so must cast to str
    println(int::str(sum));
-   sum = last + curr;
+   sum  = last + curr;
    last = curr;
    curr = sum;
 
